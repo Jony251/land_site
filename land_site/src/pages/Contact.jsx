c29f54d4a8a12057d13b500a430fe1cab0b96678
Jont251 <nevgeny90@gmail.com>
2026-02-17 14:52:51 +0200

    fix

--- land_site/src/pages/Contact.jsx
+++ land_site/src/pages/Contact.jsx
@@ -1,26 +1,6 @@
-import { useEffect } from 'react';
-import { useI18n } from '../i18n/LanguageProvider';
 import ContactSection from '../Components/ContactSection/ContactSection.comp';
 
 const Contact = () => {
-  const { t } = useI18n();
-
-  useEffect(() => {
-    const prevOverflow = document.body.style.overflow;
-    const prevPaddingRight = document.body.style.paddingRight;
-
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-    document.body.style.overflow = 'hidden';
-    if (scrollbarWidth > 0) {
-      document.body.style.paddingRight = `${scrollbarWidth}px`;
-    }
-
-    return () => {
-      document.body.style.overflow = prevOverflow;
-      document.body.style.paddingRight = prevPaddingRight;
-    };
-  }, []);
-
   return (
     <main className="contact-route">
       <div className="page-content" style={{ textAlign: 'center' }}>
